25f6fa15f77e2018501624551937ebbf7d3c75cc
ErikGarciaB <erikgarciab2001@gmail.com>
2021-03-21 20:35:59 +0100

    Primer Commit: Creacion del proyecto. Implementación del FireBase

---
+++ app/src/main/java/net/iessochoa/erikgarciabelen/gamefever/LoggingActivity.java
@@ -0,0 +1,14 @@
+package net.iessochoa.erikgarciabelen.gamefever;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoggingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logging);
+    }
+}
